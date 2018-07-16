5fe9c5a3d206f872ec77fcc9c212aa5ff0ce0d6a
Jonathan Aharon <jonathan.aharon@experitest.local>
2018-07-16 12:25:14 +0300

    first

---
+++ src/test/java/AppiumGridMaven/AppiumGridMaven/AndroidTest.java
@@ -0,0 +1,52 @@
+package AppiumGridMaven.AppiumGridMaven;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidTest {
+
+    private String accessKey = System.getenv("access_key");
+	protected AndroidDriver<AndroidElement> driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("testName", "Grid Demo - Android");
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.eribank/com.experitest.ExperiBank.LoginActivity");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.eribank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.experitest.ExperiBank.LoginActivity");
+        driver = new AndroidDriver<>(new URL("https://sales.experitest.com:443/wd/hub"), dc);
+    }
+
+    @Test
+    public void quickStartAndroidNativeDemo() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
+        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Switzerland']")).click();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@text='Yes']")).click();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/AppiumGridMaven/AppiumGridMaven/IOSTest.java
@@ -0,0 +1,51 @@
+package AppiumGridMaven.AppiumGridMaven;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class IOSTest {
+
+	private String accessKey = System.getenv("access_key");
+    protected IOSDriver<IOSElement> driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("testName", "Grid Demo - iOS");
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@os='ios' and @category='PHONE'");
+        dc.setCapability(MobileCapabilityType.APP, "cloud:com.experitest.ExperiBank");
+        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
+        driver = new IOSDriver<>(new URL("https://sales.experitest.com:443/wd/hub"), dc);
+    }
+
+    @Test
+    public void quickStartiOSNativeDemo() {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+        driver.findElement(By.xpath("//*[@id='makePaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("0541234567");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("Jon Snow");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("50");
+        driver.findElement(By.xpath("//*[@id='countryButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Switzerland']")).click();
+        driver.findElement(By.xpath("//*[@id='sendPaymentButton']")).click();
+        driver.findElement(By.xpath("//*[@id='Yes']")).click();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
